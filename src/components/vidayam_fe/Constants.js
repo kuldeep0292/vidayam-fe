2f713ff3256fd6d6c4a77bbcba3b66ded06bc6d4
Kuldeep <kuldeep@Kuldeeps-MacBook-Air.local>
2021-01-17 02:57:21 +0530

    updating server url

--- src/components/vidayam_fe/Constants.js
+++ src/components/vidayam_fe/Constants.js
@@ -1,2 +1,4 @@
-export const API_URL = 'http://localhost:8080'
-export const JPA_API_URL = 'http://localhost:8080/jpa'
+// export const API_URL = 'http://localhost:5000'
+// export const JPA_API_URL = 'http://localhost:5000/jpa'
+export const API_URL = 'http://vidayambackendprod-env.eba-2t6nye3c.us-east-2.elasticbeanstalk.com'
+export const JPA_API_URL = 'http://vidayambackendprod-env.eba-2t6nye3c.us-east-2.elasticbeanstalk.com/jpa'
