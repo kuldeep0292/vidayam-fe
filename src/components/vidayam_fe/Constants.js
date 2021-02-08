b372086910e9847ecd0ab66d4ac17dcd6cd577da
Kuldeep <kuldeep@Kuldeeps-MacBook-Air.local>
2021-02-08 22:49:51 +0530

    u

--- src/components/vidayam_fe/Constants.js
+++ src/components/vidayam_fe/Constants.js
@@ -1,4 +1,4 @@
-// export const API_URL = 'http://localhost:5000'
-// export const JPA_API_URL = 'http://localhost:5000/jpa'
-export const API_URL = 'http://vidayambackendprod-env.eba-2t6nye3c.us-east-2.elasticbeanstalk.com'
-export const JPA_API_URL = 'http://vidayambackendprod-env.eba-2t6nye3c.us-east-2.elasticbeanstalk.com/jpa'
+// export const API_URL = 'http://localhost:8080'
+// export const JPA_API_URL = 'http://localhost:8080/jpa'
+export const API_URL = 'http://13.59.23.235'
+export const JPA_API_URL = 'http://13.59.23.235/jpa'
