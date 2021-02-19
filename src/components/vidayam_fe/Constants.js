b79f4bcbbd0c47836763b163450b4163fb3e657c
Kuldeep <kuldeep@Kuldeeps-MacBook-Air.local>
2021-02-19 23:24:53 +0530

    updating constants

--- src/components/vidayam_fe/Constants.js
+++ src/components/vidayam_fe/Constants.js
@@ -1,4 +1,4 @@
 // export const API_URL = 'http://localhost:8080'
 // export const JPA_API_URL = 'http://localhost:8080/jpa'
-export const API_URL = 'http://13.59.23.235'
-export const JPA_API_URL = 'http://13.59.23.235/jpa'
+export const API_URL = 'http://3.17.39.204'
+export const JPA_API_URL = 'http://3.17.39.204/jpa'
